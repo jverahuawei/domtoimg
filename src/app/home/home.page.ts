4696b5f0ba872f797b751b8f7c54551fc01c5d2a
jose vera <jose.vera@huawei.com>
2020-07-08 00:13:04 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(){
+  }
 
+  public draw(){
+    console.log('inside draw');
+
+    html2canvas(document.querySelector('#my-node')).then(canvas => {
+      document.body.appendChild(canvas);
+    }).then(() => {
+      const canvas = document.querySelector('canvas');
+      canvas.style.display = 'none';
+      const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+      const a = document.createElement('a');
+      a.setAttribute('download', 'myImage.png');
+      console.log('dataUrl:', image);
+      a.setAttribute('href', image);
+      a.click();
+    });
+  }
 }
